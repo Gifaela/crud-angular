d16720b4eeaa6a5cb72c2bcb3d9d6cda2d7b688b
Gifaela <giulia-rafa@hotmail.com>
2022-08-02 11:35:44 -0300

    Crud completo

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl = "https://sheet.best/api/sheets/40377dfe-b7c5-4ab5-99b1-b9d1cb5ef59c";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+  constructor(private httpClient: HttpClient) { }
+
+  // C.R.U.D  CREATE, READ, UPDATE, DELETE
+
+  // Retorna a lista de usuários READ 
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.apiUrl);
+  }
+
+  // Lista usuário uníco 
+  getUser(id: string): Observable<User[]>{
+    return this.httpClient.get<User[]>(`${this.apiUrl}/id/${id}`)
+  }
+
+  //Salva usuário no banco CREATE
+  postUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions);
+  }
+
+  // Edita usuário UPDATE
+  putUser(id: string, user: User): Observable<User> {
+    return this.httpClient.put<User>(`${this.apiUrl}/id/${id}`, user, this.httpOptions);
+  }
+
+  // Excluir o usuário do banco DELETE
+  deleteUser(id: number): Observable<User> {
+    return this.httpClient.delete<User>(`${this.apiUrl}/id/${id}`)
+  }
+
+}
